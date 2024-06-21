f96ccbb8032c2d0e42cfae4c89e77ff2d7165023
HackerSinhos <contatohackersao@hotmail.com>
2024-06-21 20:52:13 -0300

    arruma o arquivo e agora o build.cmd realmente funciona
    Desculpa.

--- build.cmd
+++ build.cmd
@@ -1,17 +1,19 @@
 @echo off
 title Fazendo o arquivo .mcpack ...
 
-7z a sons-muito-altos-addon.mcpack
+7z a sons-muito-altos-addon.zip
 
 if exist ".git" ( 
-  7z d sons-muito-altos-addon.mcpack .git
-  7z d sons-muito-altos-addon.mcpack build.cmd
-  7z d sons-muito-altos-addon.mcpack 7z.exe
-  7z d sons-muito-altos-addon.mcpack README.md
+  7z d sons-muito-altos-addon.zip .git
+  7z d sons-muito-altos-addon.zip build.cmd
+  7z d sons-muito-altos-addon.zip 7z.exe
+  7z d sons-muito-altos-addon.zip README.md
+  rename sons-muito-altos-addon.zip sons-muito-altos-addon.mcpack
   exit
 ) else (
-  7z d sons-muito-altos-addon.mcpack build.cmd
-  7z d sons-muito-altos-addon.mcpack 7z.exe
-  7z d sons-muito-altos-addon.mcpack README.md
+  7z d sons-muito-altos-addon.zip build.cmd
+  7z d sons-muito-altos-addon.zip 7z.exe
+  7z d sons-muito-altos-addon.zip README.md
+  rename sons-muito-altos-addon.zip sons-muito-altos-addon.mcpack
   exit
 )
