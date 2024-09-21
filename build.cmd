6a9034b4692126dd904485eb06fc6bcafbf5efa6
HackerSinhos <73496225+HackerSinhos@users.noreply.github.com>
2024-09-21 16:25:14 -0300

    remove os dois novos arquivos

--- build.cmd
+++ build.cmd
@@ -8,12 +8,16 @@ if exist ".git" (
   7z d sons-muito-altos-addon.zip build.cmd
   7z d sons-muito-altos-addon.zip 7z.exe
   7z d sons-muito-altos-addon.zip README.md
+  7z d build.sh
+  7z d .gitignore
   rename sons-muito-altos-addon.zip sons-muito-altos-addon.mcpack
   exit
 ) else (
   7z d sons-muito-altos-addon.zip build.cmd
   7z d sons-muito-altos-addon.zip 7z.exe
   7z d sons-muito-altos-addon.zip README.md
+  7z d build.sh
+  7z d .gitignore
   rename sons-muito-altos-addon.zip sons-muito-altos-addon.mcpack
   exit
 )
